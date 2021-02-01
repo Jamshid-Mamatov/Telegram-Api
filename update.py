8b78c42e17287d56550b0c34ef1c777ee28755af
Jamshid <mamatovjamshid506@gmail.com>
2021-02-01 10:20:13 +0500

    refresh

--- update.py
+++ update.py
@@ -1,9 +1,18 @@
 import requests
 import json
 from pprint import pprint
-token = '1601845406:AAFjsTNp2FSn-Et3Vamlhv1xkX9RaRvt_WE'
-url = f'https://api.telegram.org/bot{token}/getUpdates'
-r = requests.get(url)
+
+token='1601845406:AAFjsTNp2FSn-Et3Vamlhv1xkX9RaRvt_WE'
+
+url=f'https://api.telegram.org/bot{token}/getUpdates'
+
+r=requests.get(url)
 print(r.url)
-data = r.json()
-updates = data['result']
+data=r.json()
+
+pprint(data)
+
+
+
+
+    
