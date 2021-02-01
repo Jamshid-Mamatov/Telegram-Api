8901ddc33c5c26464fe3b837200a277c9f3ec964
Jamshid <mamatovjamshid506@gmail.com>
2021-02-01 13:04:08 +0500

    refresh

--- task_update.py
+++ task_update.py
@@ -1,18 +1,25 @@
 import requests
 import json
 from pprint import pprint
-token = '1601845406:AAFjsTNp2FSn-Et3Vamlhv1xkX9RaRvt_WE'
-url = f'https://api.telegram.org/bot{token}/getUpdates'
-r = requests.get(url)
-# print(r.url)
-data = r.json()
-updates = data['result']
-for update in updates:
-    user=update['message']['from']
-    if user.get('last_name'):
-        first_name=user['first_name']
-        last_name=user['last_name']
-        print(last_name,first_name)
+
+token='1601845406:AAFjsTNp2FSn-Et3Vamlhv1xkX9RaRvt_WE'
+
+url=f'https://api.telegram.org/bot{token}/getUpdates'
+
+r=requests.get(url)
+
+data=r.json()['result']
+
+for i in data:
+    user=i['message']['from']
+
+    first_name=user.get('first_name',"")
+
+    last_name=user.get('last_name',"")
+
+    print(f"{first_name}  {last_name}")
+
+
 
     
  
