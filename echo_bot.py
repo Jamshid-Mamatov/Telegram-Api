3dcc0df9375fa9d67cc01aece0ba58fce6057815
Jamshid <mamatovjamshid506@gmail.com>
2021-02-03 00:32:17 +0500

    reply echo bot

--- echo_bot.py
+++ echo_bot.py
@@ -2,15 +2,16 @@ import requests
 
 import json
 
-token="1636539811:AAE7pYJmxdLFXTLD32Ery6EXFFJwRRbqYWs"
+#@echo_jamshid_bot
 
-def sendMsg(id,text):
+token="1407685639:AAH8rpMbq64izZqkxxDieN6MpQof-DOCmIk"
+
+def sendMsg(id,text,msg_id):
     url=f"https://api.telegram.org/bot{token}/sendMessage"
-
+    
     payload={
-        'chat_id':id,
-        'text':text
-
+        
+        'reply_to_message_id':msg_id
     }
     r=requests.get(url,params=payload)
     
@@ -22,18 +23,20 @@ def update():
     r=requests.get(url).json()
     data=r['result']
     message=data[-1]['message']['text']
-    id=data[-1]['message']['from']['id']
-    date=data[-1]['message']['date']
-    return message,id,date
+    id=data[-1]['message']['chat']['id']
+    msg_id=data[-1]['message']['message_id']
+    return message,id,msg_id
 
 
-last_date=None
+last_msg_id=None
 while True :
     
-    text,id,date=update()
-    if last_date!=date:
-        sendMsg(id,text)
-        last_date=date
+    text,id,msg_id=update()
+    
+    if last_msg_id!=msg_id:
+        last_msg_id=msg_id
+        sendMsg(id,text,msg_id)
+        
     
 
 
