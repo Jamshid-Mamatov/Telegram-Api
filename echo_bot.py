502b6e60cd289ae8b4fd00d2db55f11e46621bc6
Jamshid <mamatovjamshid506@gmail.com>
2021-02-02 10:56:54 +0500

    send all messages in echo bot

--- echo_bot.py
+++ echo_bot.py
@@ -2,22 +2,26 @@ import requests
 
 import json
 
-
-
 token="1636539811:AAE7pYJmxdLFXTLD32Ery6EXFFJwRRbqYWs"
 
+def sendMsg(id,text):
+    url=f"https://api.telegram.org/bot{token}/sendMessage"
+
+    payload={
+        'chat_id':id,
+        'text':text
+    }
+    r=requests.get(url,params=payload)
+    print(r.url)
+
+
 url=f"https://api.telegram.org/bot{token}/getUpdates"
-
 r=requests.get(url).json()
+data=r['result']
+user_msg={}
+ids=[]
+for i in data:
+    id=i['message']['from']['id']
+    text=i['message']['text']
+    sendMsg(id,text)
 
-data=r['result'][-1]
-id=data['message']['from']['id']
-text=data['message']['text']
-
-url=f"https://api.telegram.org/bot{token}/sendMessage"
-
-payload={
-    'chat_id':id,
-    'text':text
-}
-r=requests.get(url,params=payload)
