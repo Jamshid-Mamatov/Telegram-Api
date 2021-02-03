0bc8aafa2136614b06a447e5e332f799cbb488e0
Jamshid <mamatovjamshid506@gmail.com>
2021-02-03 21:11:50 +0500

    echo bot added photo return

--- echo_bot.py
+++ echo_bot.py
@@ -27,6 +27,16 @@ def stikerSend(id,file_id,msg_id):
     r=requests.get(url,params=payload)
 
 
+def sendphoto(chat_id,message_photo,msg_id):
+    url=f"https://api.telegram.org/bot{token}/sendPhoto"
+
+    payload={
+        'chat_id':chat_id,
+        'photo':message_photo,
+        'reply_to_message_id':msg_id
+    }
+    r=requests.get(url,params=payload)
+
 
 def update():
 
@@ -34,13 +44,22 @@ def update():
     
     r=requests.get(url).json()
     data=r['result']
+    
     message=data[-1]['message'].get('text')
+
     message_stckr=data[-1]['message'].get('sticker')
+
+    message_photo=data[-1]['message'].get('photo')
+
+    if message_photo!=None:
+        message_photo=message_photo[0]['file_id']
     if message_stckr!=None:
         message_stckr=message_stckr['file_id']
-    id=data[-1]['message']['chat']['id']
+
+    chat_id=data[-1]['message']['chat']['id']
     msg_id=data[-1]['message']['message_id']
-    return message,message_stckr,id,msg_id
+    inform=[message,message_stckr,message_photo,chat_id,msg_id]
+    return inform
 
 
 
@@ -48,14 +67,21 @@ def update():
 last_msg_id=None
 while True :
     
-    text,message_stckr,chat_id,msg_id=update()
-    
+    data=update()
+    text=data[0]
+    message_stckr=data[1]
+    message_photo=data[2]
+    chat_id=data[3]
+    msg_id=data[4]
     if last_msg_id!=msg_id:
         last_msg_id=msg_id
         if text!=None :
             sendMsg(chat_id,text,msg_id)
-        else:
+        elif message_stckr!=None :
             stikerSend(chat_id,message_stckr,msg_id)
+        else:
+            sendphoto(chat_id,message_photo,msg_id)
+
         
     
 
