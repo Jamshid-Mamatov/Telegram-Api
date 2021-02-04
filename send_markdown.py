8de01da38fe69d6d911d5a2e45276d96598b6fb8
Jamshid <mamatovjamshid506@gmail.com>
2021-02-04 16:54:10 +0500

    MarkdownV2

--- send_markdown.py
+++ send_markdown.py
@@ -1,3 +1,19 @@
 import os
+import requests
 from pprint import pprint 
-print(os.environ['token'])
+token=os.environ['token']
+
+def sendMsg(markdown_list):
+    url=f'https://api.telegram.org/bot{token}/sendMessage'
+    for text in markdown_list:
+        payload={
+            'chat_id':937691492,
+            'text':text,
+            'parse_mode':'MarkdownV2'
+        }
+        r=requests.get(url,payload)
+
+markdown=['_Mamatov_','*Jamshid*','__python__','~tuit~','*_~__student__~_*',
+'[Jamshid](https://t.me/Jamshid_Mamatov)','[codeschool](https://t.me/codeschooluz)']
+sendMsg(markdown)
+
